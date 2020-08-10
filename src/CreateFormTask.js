b390540a9217dbf09ce2a38897b9474168eb070e
TodoList <petkooleg@gmail.com>
2020-08-10 16:11:05 +0300

    edit 3 files

---
+++ src/CreateFormTask.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CreateFormTask = (props) => {
+    return (
+        <div>
+            CreateFormTask
+            <input/>
+            <button>create new task</button>
+
+        </div>
+    )
+
+}
+
+export default CreateFormTask
